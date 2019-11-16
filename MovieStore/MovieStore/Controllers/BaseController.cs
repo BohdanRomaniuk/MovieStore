509c31486659fadd547cd75d032936323b01dad3
BohdanRomaniuk <bohdan2307@gmail.com>
2019-11-16 22:27:57 +0200

    Working API level

---
+++ MovieStore/MovieStore/Controllers/BaseController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieStore.Services;
+
+namespace MovieStore.Controllers
+{
+    public abstract class BaseController<TEntity> : Controller
+    {
+        protected IService<TEntity> _service;
+
+        public BaseController(IService<TEntity> service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public virtual IActionResult Get()
+        {
+            var value = _service.Get();
+            return Ok(value);
+        }
+
+        [HttpGet("{id}")]
+        public virtual IActionResult Get(int id)
+        {
+            var value = _service.Get(id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public virtual IActionResult Post([FromBody]TEntity entity)
+        {
+            if (ModelState.IsValid)
+            {
+                _service.Add(entity);
+                return CreatedAtRoute(Url, entity);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public virtual IActionResult Delete(int id)
+        {
+            _service.Remove(id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public virtual IActionResult Put([FromBody]TEntity entity)
+        {
+            if (ModelState.IsValid)
+            {
+                _service.Update(entity);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ MovieStore/MovieStore/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieStore.DataAccess;
+using MovieStore.Services;
+
+namespace MovieStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : BaseController<Movie>
+    {
+        public MovieController(IMovieService service)
+            : base(service)
+        {
+        }
+    }
+}
