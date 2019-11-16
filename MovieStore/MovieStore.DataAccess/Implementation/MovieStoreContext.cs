ac62ac4114950a90e6b712599e3ea91817963d71
BohdanRomaniuk <bohdan2307@gmail.com>
2019-11-16 19:54:16 +0200

    Create Initial DataAccess

---
+++ MovieStore/MovieStore.DataAccess/Implementation/MovieStoreContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieStore.DataAccess
+{
+    public class MovieStoreContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        public MovieStoreContext(DbContextOptions<MovieStoreContext> options) :
+            base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Movie>().ToTable("Movies");
+        }
+    }
+}
