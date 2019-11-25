b9cbb49d0a7f843362dac23650c9b08445f51770
BohdanRomaniuk <bohdan2307@gmail.com>
2019-11-25 22:31:41 +0200

    Extend movie model & add movie page on front-end

--- back-end/MovieStore.DataAccess/Models/Movie.cs
+++ back-end/MovieStore.DataAccess/Models/Movie.cs
@@ -23,7 +23,25 @@ namespace MovieStore.DataAccess
         public int Year { get; set; }
 
         [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
         public string Story { get; set; }
+
+        [Required]
+        public string Length { get; set; }
+
+        [Required]
+        public string Companies { get; set; }
+
+        [Required]
+        public string Director { get; set; }
+
+        [Required]
+        public string Actors { get; set; }
 
         public List<Comment> Comments { get; set; }
 
