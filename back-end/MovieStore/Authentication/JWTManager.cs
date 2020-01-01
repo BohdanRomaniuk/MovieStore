4f972d09077abf0b6913778d1f54e581d8c2f942
BohdanRomaniuk <bohdan2307@gmail.com>
2020-01-01 16:06:26 +0200

    Migration to .Net Core 3.1 & .Net Standard 2.1

--- back-end/MovieStore/Authentication/JWTManager.cs
+++ back-end/MovieStore/Authentication/JWTManager.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿//using Microsoft.IdentityModel.Tokens;
 using MovieStore.AuthHelpers;
 using System;
 using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
+//using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -14,14 +14,15 @@ namespace MovieStore.Authentication
         public static string GenerateToken(ClaimsIdentity identity)
         {
             var now = DateTime.UtcNow;
-            var jwt = new JwtSecurityToken(
-                    issuer: JWTConfigurator.ISSUER,
-                    audience: JWTConfigurator.AUDIENCE,
-                    notBefore: now,
-                    claims: identity.Claims,
-                    expires: now.Add(TimeSpan.FromMinutes(JWTConfigurator.LIFETIME)),
-                    signingCredentials: new SigningCredentials(JWTConfigurator.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
-            return new JwtSecurityTokenHandler().WriteToken(jwt);
+            //var jwt = new JwtSecurityToken(
+            //        issuer: JWTConfigurator.ISSUER,
+            //        audience: JWTConfigurator.AUDIENCE,
+            //        notBefore: now,
+            //        claims: identity.Claims,
+            //        expires: now.Add(TimeSpan.FromMinutes(JWTConfigurator.LIFETIME)),
+            //        signingCredentials: new SigningCredentials(JWTConfigurator.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            //return new JwtSecurityTokenHandler().WriteToken(jwt);
+            return string.Empty;
         }
     }
 }
